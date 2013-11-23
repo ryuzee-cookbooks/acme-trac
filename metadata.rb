87ecb322808d61b2f3e0b0ef9119b36c4f93ed63
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-24 06:21:05 +0900

    clarify license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'acme-trac'
 maintainer       'Ryutaro YOSHIBA'
 maintainer_email 'ryuzee@gmail.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures acme-trac'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
